9174584453e89208d451faf5c6074decb8e67422
ruboarm <ruboarm@gmail.com>
2022-05-29 21:48:19 +0400

    Create Sales by Match.cs

---
+++ Algorithms/Implementation/Sales by Match.cs
@@ -0,0 +1,62 @@
+// Problem:   https://www.hackerrank.com/challenges/sock-merchant/problem
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        List<int> unique = ar.Distinct().OrderBy(a => a).ToList();
+
+        int pairs = 0;
+        foreach (var item in unique)
+        {
+            pairs += ar.Count(i => i == item) / 2;
+        }
+
+        return pairs;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
