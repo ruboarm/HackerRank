ff96f83c36311e2c1825c608a33a3805c8d5db48
ruboarm <ruboarm@gmail.com>
2022-05-29 21:50:53 +0400

    Create Drawing Book.cs

---
+++ Algorithms/Implementation/Drawing Book.cs
@@ -0,0 +1,63 @@
+// Problem:   https://www.hackerrank.com/challenges/drawing-book/problem
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p)
+    {
+        int nNumber = n % 2 == 0 ? n : n - 1;
+        int pNumber = p % 2 == 0 ? p : p - 1;
+
+        if (nNumber == 0 || pNumber == nNumber)
+            return 0;
+
+        if (pNumber > nNumber / 2)
+            return (nNumber - pNumber) / 2;
+        else
+            return pNumber / 2;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.pageCount(n, p);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
