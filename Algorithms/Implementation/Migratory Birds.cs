4721e20a208a1550c34601613c86868d7ff769e3
ruboarm <ruboarm@gmail.com>
2022-05-30 00:04:24 +0400

    Create Migratory Birds.cs

---
+++ Algorithms/Implementation/Migratory Birds.cs
@@ -0,0 +1,59 @@
+// Problem:   https://www.hackerrank.com/challenges/migratory-birds/problem
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        List<int> counts = new List<int>(5) {0,0,0,0,0};
+        
+        foreach(var i in arr){
+            counts[i+1]++;
+        }
+        
+		// Compiler Error
+        return counts.Where(i=>i==counts.Max());
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
