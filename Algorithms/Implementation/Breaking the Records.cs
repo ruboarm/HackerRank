0614cc0813305ffaf0324bb89fc1ea92b59158d7
ruboarm <ruboarm@gmail.com>
2022-05-29 23:02:56 +0400

    Create Breaking the Records.cs

---
+++ Algorithms/Implementation/Breaking the Records.cs
@@ -0,0 +1,69 @@
+// Problem:   https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> breakingRecords(List<int> scores)
+    {
+        int minVal = scores[0];
+        int maxVal = scores[0];
+        
+        int minRecCount = 0;
+        int maxRecCount = 0;
+        
+        foreach(var number in scores){
+            if(minVal > number){
+                minVal = number;
+                minRecCount++;
+            }
+            if(maxVal < number){
+                maxVal = number;
+                maxRecCount++;
+            }
+        }
+        
+        return new List<int>(){ maxRecCount, minRecCount};
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+        List<int> result = Result.breakingRecords(scores);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
