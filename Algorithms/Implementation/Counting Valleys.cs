daadaa2f826f2b674e8659981f2acf4b6be2ae72
ruboarm <ruboarm@gmail.com>
2022-06-04 16:25:39 +0400

    Create Counting Valleys.cs

---
+++ Algorithms/Implementation/Counting Valleys.cs
@@ -0,0 +1,73 @@
+// Problem:   https://www.hackerrank.com/challenges/counting-valleys/problem
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int valleys=0;
+        bool countValleys=true;
+        
+        int level=0;
+        foreach(var c in path){
+            if(c=='D') // Down
+                level--;
+            else // Up
+                level++;
+                
+            if(level<0 && countValleys){
+                valleys++;
+                countValleys=false;
+            }
+
+            if(level==0)            
+                countValleys=true;
+        }
+        
+        return valleys;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
