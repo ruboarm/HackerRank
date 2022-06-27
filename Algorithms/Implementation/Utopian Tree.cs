5f95a733deee7f0c59c2fb48ae9e7498720ae855
ruboarm <ruboarm@gmail.com>
2022-06-28 00:05:39 +0400

    Create Utopian Tree.cs

---
+++ Algorithms/Implementation/Utopian Tree.cs
@@ -0,0 +1,69 @@
+// Problem:  https://www.hackerrank.com/challenges/utopian-tree/problem
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int utopianTree(int n)
+    {
+        // Set initial height
+        int height = 1;
+        
+        // If index is even (index%2==0) then double the height (Spring)
+        // If index is odd (index%2!=0) then increase the height by 1(Summer)
+        for(int i=0; i<=n; i++){
+            if(i>0){
+                if(i%2!=0)
+                    height*=2;
+                else
+                    height++;
+            }
+        }
+        
+        return height;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.utopianTree(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
